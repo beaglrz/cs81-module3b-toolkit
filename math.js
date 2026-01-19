399759d37e73f4ec43df584cfc6329733483ce20
Beatriz Galarza <beatriz.glrz@gmail.com>
2026-01-19 00:45:39 -0800

    Added isEven function and results

--- math.js
+++ math.js
@@ -1,9 +1,14 @@
 //Add double function
 function double(n) {
-  return n * 2;
+return n * 2;
 }
 
 //Add square function
 function square(n) {
-  return n * n;
+return n * n;
+}
+
+//Add Even function
+function isEven(n) {
+return n% 2 === 0;
 }
